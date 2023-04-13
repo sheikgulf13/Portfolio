f821952983cfefec3ed83bab8f7fa92e9300e7c7
sheik gulfaan <sheikgulf01@gmail.com>
2023-04-14 01:59:48 +0530

    styling-updated

--- app/myProject.jsx
+++ app/myProject.jsx
@@ -1,6 +1,5 @@
 "use client";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -12,29 +11,24 @@ function myProject() {
         <h2 className='text-4xl font-extrabold my-5 mb-20'>My Projects</h2>
 
         <div className='grid grid-cols-2 lg:grid-cols-3 gap-20 lg:gap-36 place-items-center '>
-            <Card sx={{width: 250, height: 350}} className='Card flex flex-col justify-around items-center shadow-lg rounded-xl hover:shadow-xl hover:shadow-slate-400 bg-slate-200'>
+            <div className='Card flex flex-col justify-around items-center shadow-lg rounded-xl hover:shadow-xl hover:shadow-slate-400 bg-slate-200'>
                 <CardMedia sx={{height: 140, width: 225}} component='img' image='/wsApp-clone.png'/>
                 <Typography className='font-semibold text-lg'>WhatsApp Clone</Typography>
-                <CardActions>
                     <Button className='bg-slate-600 text-slate-50 font-bold px-5 py-3 hover:bg-slate-500 rounded-lg'>
                         <a href="https://whatsapp-clone-2cbc0.web.app/" target='blank'>View Project</a>
                     </Button>
-                </CardActions>
-            </Card>
+            </div>
 
-            <Card sx={{width: 250, height: 350}} className='Card hidden lg:flex flex-col justify-around items-center shadow-lg rounded-xl hover:shadow-xl hover:shadow-slate-400 bg-slate-200'>
+            <div className='Card hidden lg:flex flex-col justify-around items-center shadow-lg rounded-xl hover:shadow-xl hover:shadow-slate-400 bg-slate-200'>
                 <CardMedia sx={{height: 140, width: 225}} component='img' image='/ggle-clone.png'/>
                 <Typography className='font-semibold text-lg'>Google Clone</Typography>
-                <CardActions>
                     <Button className='bg-slate-600 text-slate-50 font-bold px-5 py-3 hover:bg-slate-500 rounded-lg'>
                         <a href="https://clone-gle.netlify.app/" target='blank'>View Project</a>
                     </Button>
-                </CardActions>
-            </Card>
+            </div>
 
-            <Card sx={{width: 200, height: 200}} className='Card Card-more flex flex-col justify-around items-center shadow-2xl rounded-full  hover:cursor-pointer hover:shadow-xl'>
-                <CardActions>
-                    <Button className='button text-slate-600
+            <div className='Card-more flex flex-col justify-around items-center shadow-2xl rounded-full  hover:cursor-pointer hover:shadow-xl'>
+                    <button className='button text-slate-600
                     hover:bg-transparent font-bold px-5 py-3 rounded-lg text-lg '>
                         <Link href="/projects" >
                             <div className="button-content">
@@ -47,9 +41,8 @@ function myProject() {
                             </div>
 
                         </Link>
-                    </Button>
-                </CardActions>
-            </Card>
+                    </button>
+            </div>
         </div>
     </div>
   )
